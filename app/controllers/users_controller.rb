bd218782fb9d8d2117667016e88f8bfe32452280
han.acc <devtopro@hotmail.com>
2018-02-25 15:44:30 +0000

    kadai-monolist-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,19 +22,6 @@ class UsersController < ApplicationController
     end
   end
   
-  def want(item)
-    self.wants.find_or_create_by(item_id: item.id)
-  end
-
-  def unwant(item)
-    want = self.wants.find_by(item_id: item.id)
-    want.destroy if want
-  end
-
-  def want?(item)
-    self.want_items.include?(item)
-  end
-  
   private
 
   def user_params
